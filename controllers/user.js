57a2b42162063b8a9d1d6755d7fc6d9c3750a278
Rishav Sharma <rishavtosharma@gmail.com>
2023-04-13 12:15:08 +0530

    feat: :construction: strated working on creating CRUD operations in MongoDB using Node.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,136 @@
-export const user = (req, res) => {
-    res.json("The response successfully sent!")
+import { createError } from "../error.js";
+import User from "../models/User.js";
+
+export const update = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+        try {
+            const updatedUser = await User.findByIdAndUpdate(req.params.id,
+                {
+                    $set: req.body
+                },
+                {
+                    new: true
+                }
+            )
+            res.status(200).send(updatedUser)
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        return next(createError(403, "Please check the added user Id. You can only update your account!"))
+    }
+}
+export const deleteUser = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+        try {
+            const deletedUser = await User.findByIdAndDelete(req.params.id,
+                {
+                    $set: req.body
+                },
+                {
+                    new: true
+                }
+            )
+            res.status(200).send(`User has been deleted and the user that was deleted is:  ${deletedUser}`)
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        return next(createError(403, "Please check the added user Id. You can only delete your account!"))
+    }
+}
+export const getUser = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+        try {
+            const deletedUser = await User.findByIdAndDelete(req.params.id,
+                {
+                    $set: req.body
+                },
+                {
+                    new: true
+                }
+            )
+            res.status(200).send(`The user that was deleted is ${deletedUser}`)
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        return next(createError(403, "Please check the added user Id. You can only delete your account!"))
+    }
+}
+export const subscribe = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+        try {
+            const deletedUser = await User.findByIdAndDelete(req.params.id,
+                {
+                    $set: req.body
+                },
+                {
+                    new: true
+                }
+            )
+            res.status(200).send(`The user that was deleted is ${deletedUser}`)
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        return next(createError(403, "Please check the added user Id. You can only delete your account!"))
+    }
+}
+export const unsubscribe = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+        try {
+            const deletedUser = await User.findByIdAndDelete(req.params.id,
+                {
+                    $set: req.body
+                },
+                {
+                    new: true
+                }
+            )
+            res.status(200).send(`The user that was deleted is ${deletedUser}`)
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        return next(createError(403, "Please check the added user Id. You can only delete your account!"))
+    }
+}
+export const like = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+        try {
+            const deletedUser = await User.findByIdAndDelete(req.params.id,
+                {
+                    $set: req.body
+                },
+                {
+                    new: true
+                }
+            )
+            res.status(200).send(`The user that was deleted is ${deletedUser}`)
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        return next(createError(403, "Please check the added user Id. You can only delete your account!"))
+    }
+}
+export const dislike = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+        try {
+            const deletedUser = await User.findByIdAndDelete(req.params.id,
+                {
+                    $set: req.body
+                },
+                {
+                    new: true
+                }
+            )
+            res.status(200).send(`The user that was deleted is ${deletedUser}`)
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        return next(createError(403, "Please check the added user Id. You can only delete your account!"))
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,28 @@
 import express from "express";
-import { user } from "../controllers/user.js";
+import { deleteUser, dislike, getUser, like, subscribe, unsubscribe, update } from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
-router.get("/user", user)
+// update user
+router.put("/:id", verifyToken, update);
+
+// delete user
+router.delete("/:id", verifyToken, deleteUser);
+
+// get a user
+router.put("/find/:id", getUser);
+
+// subscribe a user
+router.put("/sub/:id", verifyToken, subscribe);
+
+// unsubscribe a user
+router.put("/unsub/:id", verifyToken, unsubscribe);
+
+// like a video
+router.put("/like/:id", verifyToken, like);
+
+// dislike a video
+router.put("/dislike/:id", verifyToken, dislike);
 
 export default router;  
